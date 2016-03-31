d3f41a87c74f1df6d10abb1aaae7fe2b55a8e26c
Sarah Alsmiller <ramvling@gmail.com>
2016-03-31 12:38:29 -0400

    Have to try new OS

--- Networking/pyserver.py
+++ Networking/pyserver.py
@@ -1,13 +1,18 @@
 #from pyramid.config import Configurator
 #from pyramid.response import Response
 import socket
+import sys
 import threading
-import httplib
+import string, cgi, time
+
+sys.path.insert(0, './PyWebPlug')
+from wsserver import *
+from time import sleep
 
 #Listen for c# connection. This should probably be on a seperate thread
 HOST = '127.0.0.1'
 CPORT = 8000
-WEBPORT = 8085
+WEBPORT = 8086
 
 def setupMessages():
     m1 = createMsgStruct(1, False)
@@ -18,18 +23,21 @@ def setupMessages():
 
 #testing PyWebPlug connections
 print("listening for websocket connection on 8085")
-ws = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ws.bind( (HOST, 8085) )
-ws.listen(1)
-conn, ad = ws.accept()
-while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        print (data)
-        wait = input("enter a string:")
-        conn.sendall(wait)"""
+try:
+        setupMessages()
+        server = startServer()
+        while True:
+            newClient = handleNetwork()
+            if newClient:
+                handle(newClient)
+            for client in clients:
+                client.handle()
+            sleep(0.01)
+except KeyboardInterrupt:
+    print(' received, closing server.')
+    server.close()
 
+"""
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind( (HOST, PORT) )
 #probably need to form some sort of handshake here so I know what kind of client is connecting
@@ -49,4 +57,4 @@ def talkToC():
         connection.sendall(wait)
 
 thread = threading.Thread(target = talkToC)
-thread.start()
+thread.start()"""
